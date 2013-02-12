42b413ce13f7d06c4d81a53cd632673776051b2d
Takeshi KOMIYA <i.tkomiya@gmail.com>
2013-02-12 12:25:18 +0900

    Run foodcritic directly

--- lib/rake-foodcritic.rb
+++ lib/rake-foodcritic.rb
@@ -1,29 +1,26 @@
 require "rake-foodcritic/version"
+require "foodcritic"
 
 namespace :chef do
   desc "Run foodcritic"
   task :foodcritic do
     if Gem::Version.new("1.9.2") <= Gem::Version.new(RUBY_VERSION.dup)
-      path = File.join(%w{tmp foodcritic})
-      foodcritic_sandbox(path) do
-        sh "foodcritic --epic-fail any #{path}"
+      print "Running foodcritic ... "
+      if File.exists?('cookbooks')
+        result = FoodCritic::Linter.new.check 'cookbooks', {}
+      else
+        result = FoodCritic::Linter.new.check '.', {}
+      end
+
+      if result.failed? or result.warnings.size > 0
+        puts "FAILED"
+        puts result
+        fail
+      else
+        puts "OK"
       end
     else
       puts "WARNING: Skipped running foodcritic. Ruby 1.9.2 or higher required"
-    end
-  end
-
-  def foodcritic_sandbox(path)
-    files = %w{*.md *.rb attributes definitions files
-               libraries providers recipes resources templates}
-
-    begin
-      rm_rf path
-      mkdir_p path
-      cp_r Dir.glob("{#{files.join(',')}}"), path
-      yield
-    ensure
-      rm_rf path
     end
   end
 end
